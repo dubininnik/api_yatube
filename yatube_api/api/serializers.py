9b20d072b2143eb7ab1a2be7cb5ce9ec4e3089f8
Николай Дубинин <dubinin.kerch@yandex.ru>
2023-02-12 23:26:53 +0300

    Third commit attempt

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -11,14 +11,14 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ('__all__')
+        fields = '__all__'
 
 
 class GroupSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Group
-        fields = ('__all__')
+        fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -29,5 +29,5 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = ('__all__')
+        fields = '__all__'
         read_only_fields = ('post',)
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from posts.models import Group, Post
 
 from .permissions import IsOwnerOrAuthReadOnly
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+from posts.models import Group, Post
 
 
 class PostViewSet(viewsets.ModelViewSet):
